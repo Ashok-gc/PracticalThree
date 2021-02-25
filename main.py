514727c14777b25af443cf0cb914246f072f2b0d
Ashok-gc <78782373+Ashok-gc@users.noreply.github.com>
2021-02-25 10:19:01 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#2 write a program that reads the length of the base and the
+# height
+# right angled triangle
+# and prints the area every number is given on a separate line
+
+length = int(input("enter the value of length: "))
+height = int(input("enter he value of height: "))
+area = (length * height)//2
+print(f"The area of right angled triangle is {area} ")
